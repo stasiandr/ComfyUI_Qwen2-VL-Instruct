a1d5bc292b99c0f0db6aba27e4dc80d30f6f9b8e
ShtenDev <stasiandr@gmail.com>
2025-02-24 02:40:21 +0300

    Update util_nodes.py

--- util_nodes.py
+++ util_nodes.py
@@ -78,3 +78,36 @@ class ImageLoader:
             return "Invalid image file: {}".format(image)
 
         return True
+
+class VideoLoader:
+    @classmethod
+    def INPUT_TYPES(s):
+        input_dir = folder_paths.get_input_directory()
+        files = [f for f in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, f)) and f.split('.')[-1] in ['mov', 'mp4']]
+        return {"required":
+                    {"image": (sorted(files), {"image_upload": True})},
+                }
+
+    CATEGORY = "Comfyui_Qwen2-VL-Instruct"
+
+    RETURN_TYPES = ("PATH")
+    FUNCTION = "load_image"
+    def load_image(self, image):
+        image_path = folder_paths.get_annotated_filepath(image)
+
+        return (image_path)
+
+    @classmethod
+    def IS_CHANGED(s, image):
+        image_path = folder_paths.get_annotated_filepath(image)
+        m = hashlib.sha256()
+        with open(image_path, 'rb') as f:
+            m.update(f.read())
+        return m.digest().hex()
+
+    @classmethod
+    def VALIDATE_INPUTS(s, image):
+        if not folder_paths.exists_annotated_filepath(image):
+            return "Invalid image file: {}".format(image)
+
+        return True
